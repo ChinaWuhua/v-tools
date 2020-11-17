7013f495ea246f5c0c28c33570079952693a376a
吴华 <1007710266@qq.com>
2020-11-17 10:04:19 +0800

    代码优化 & 添加去重方法

--- js/commonTools.js
+++ js/commonTools.js
@@ -3,8 +3,6 @@ const Vtools = (function () {
     /*
       冒泡排序法
       传入一维数组,会得到排序后的数组
-      params: []
-      result: []
     */
     Bubbling (arr) {
       for(let i=0; i<arr.length; i++){
@@ -22,8 +20,6 @@ const Vtools = (function () {
     /*
       数组扁平化
       传入嵌套了任意层级的数组，会得到一个扁平化的数组
-      params: []
-      result: []
     */
     Flattening (data) {
       let res = []
@@ -34,6 +30,21 @@ const Vtools = (function () {
       } else {
         res = res.concat(data) 
       }
+      return res
+    },
+
+    /*
+      #todo - 未测试
+      数据去重
+      传入一维数组，会得到一个去重后的数组
+      data: 目标数组
+      tag: 可用于筛选是否重复的标签
+    */
+    noRepeat (data) {
+      let res = []
+      data.forEach(item => {
+        if (!res.includes(item)) res.push(item)
+      })
       return res
     },
   }
